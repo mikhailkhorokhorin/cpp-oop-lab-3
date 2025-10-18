a829a6880039be9c4835f282e6fb8984f77684da
mikhailkhorokhorin <mikhailkhorokhorin@gmail.com>
2025-10-18 18:09:44 +0300

    Add tests files and main

---
+++ src/include/array.h
@@ -0,0 +1,34 @@
+#include <iostream>
+
+#include "figure.h"
+
+class Array {
+   private:
+    Figure** figures_;
+    size_t size_;
+    size_t capacity_;
+
+    void resize(size_t capacity);
+
+   public:
+    explicit Array(size_t capacity = 2);
+
+    Array(const Array& other);
+    Array(Array&& other) noexcept;
+
+    Array& operator=(Array other);
+    Array& operator=(Array&& other) noexcept;
+    ~Array();
+
+    void addFigure(Figure* figure);
+    void removeFigure(size_t index);
+    Figure* getFigure(size_t index) const;
+
+    size_t getSize() const;
+    size_t getCapacity() const;
+
+    double getAllArea() const;
+    void printFigures() const;
+
+    Figure* operator[](size_t index) const;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,42 @@
-#include "../include/point.h
+#include "include/array.h"
+#include "include/point.h"
+#include "include/rectangle.h"
+#include "include/square.h"
+#include "include/trapezoid.h"
 
 int main() {
+    Square square({0, 0}, {2, 0}, {2, 2}, {0, 2});
+    Rectangle rectangle({0, 0}, {3, 0}, {3, 2}, {0, 2});
+    Trapezoid trapezoid({0, 0}, {4, 0}, {3, 2}, {1, 2});
+
+    std::cout << "Square: ";
+    square.print(std::cout);
+    std::cout << "Center: (" << square.getCenter().x << ", " << square.getCenter().y << ") ";
+    std::cout << "Area: " << static_cast<double>(square) << std::endl;
+
+    std::cout << "Rectangle: ";
+    rectangle.print(std::cout);
+    std::cout << "Center: (" << rectangle.getCenter().x << ", " << rectangle.getCenter().y << ") ";
+    std::cout << "Area: " << static_cast<double>(rectangle) << std::endl;
+
+    std::cout << "Trapezoid: ";
+    trapezoid.print(std::cout);
+    std::cout << "Center: (" << trapezoid.getCenter().x << ", " << trapezoid.getCenter().y << ") ";
+    std::cout << "Area: " << static_cast<double>(trapezoid) << std::endl << std::endl;
+
+    Array arr(3);
+    arr.addFigure(new Square(square));
+    arr.addFigure(new Rectangle(rectangle));
+    arr.addFigure(new Trapezoid(trapezoid));
+
+    arr.printFigures();
+
+    std::cout << "Total area: " << arr.getAllArea() << std::endl;
+
+    Figure* figure = arr[1];
+    std::cout << "Figure at index 1: ";
+    figure->print(std::cout);
+    std::cout << "Area: " << static_cast<double>(*figure) << std::endl;
+
+    return 0;
 }
